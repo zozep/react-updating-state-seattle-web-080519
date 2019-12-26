79b60fdf01f6fb74778e46722f0e5ba386c56265
joseph park <jpark041@gmail.com>
2019-12-26 14:39:00 -0800

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,42 @@
 // Code ClickityClick Component Here
+import React from "react";
+
+class ClickityClick extends React.Component {
+  constructor() {
+    super();
+    // Define the initial state:
+    this.state = {
+      handleClick: false,
+      theme: "blue",
+      addressInfo: {
+        street: null,
+        number: null,
+        city: "New York City",
+        country: null
+      }
+    };
+  }
+
+  handleClick = () => {
+    this.setState(
+      {
+        handleClick: true,
+        currentTheme: "red",
+        addressInfo: Object.assign({}, this.state.addressInfo, {
+          city: "NOT New York City"
+        })
+      },
+      () => console.log(this.state.handleClick)
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <p>I have {this.state.handleClick ? null : "not"} been clicked!</p>
+        <button onClick={this.handleClick}>Click me!</button>
+      </div>
+    );
+  }
+}
+export default ClickityClick;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import ClickityClick from "./components/ClickityClick";
 ReactDOM.render(
   <div>
-    Mount Components Here
+    <ClickityClick />
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
